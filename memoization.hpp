9c197a3bf8b66223eb937ad153c03a6fb2b725e2
fienestar <fienestar@gmail.com>
2022-08-22 01:24:03 +0900

    Update memoization.hpp

--- memoization.hpp
+++ memoization.hpp
@@ -1,57 +1,72 @@
 #ifndef FIENE_MEMOIZATION_HPP
 #define FIENE_MEMOIZATION_HPP
 
+#include <functional>
 #include <map>
 #include <tuple>
 
-namespace fiene {
-    namespace {
-        template <class function_t>
-        struct memo_map_helper;
-
-        template <class return_t, class ...arg_t>
-        struct memo_map_helper<return_t(*)(arg_t...)>:memo_map_helper<return_t(arg_t...)>
-        {};
-
-        template <class return_t, class ...arg_t>
-        struct memo_map_helper<return_t(arg_t...)>
-        {
-            using type = std::map<std::tuple<arg_t...>, return_t>;
-        };
-    }
-
-    template <auto f, class map = typename memo_map_helper<decltype(f)>::type>
-    class memo_t
+namespace fiene{
+    template <class Function, class ...Args>
+    class memoize_wrapper
     {
-        static map memo_map;
+        Function m_function;
+        std::map<std::tuple<Args...>, decltype(m_function(std::declval<Args>()...))> m_memo;
     public:
-        template <class ...Args>
-        bool has(Args&&... args)
-        {
-            return memo_map.find(std::make_tuple(std::forward<Args>(args)...)) != memo_map.end();
-        }
+        memoize_wrapper(Function function)
+        : m_function(function), m_memo() {}
 
-        template <class ...Args>
-        auto operator()(Args&&... args)
+        auto operator()(Args&& ...args)
         {
-            auto arg_tuple = std::make_tuple(std::forward<Args>(args)...);
-            auto it = memo_map.find(arg_tuple);
-            if (it != memo_map.end())
+            auto args_tuple = std::make_tuple<Args...>(std::move(args)...);
+            auto it = m_memo.find(args_tuple);
+            if(it != m_memo.end())
                 return it->second;
-            auto r = f(args...);
-            memo_map.emplace(std::make_pair(arg_tuple, r));
-            return r;
+            else {
+                auto result = m_function(std::forward<Args>(args)...);
+                m_memo.emplace(std::move(args_tuple), result);
+                return result;
+            }
         }
     };
 
-    template <auto f, class map>
-    map memo_t<f, map>::memo_map;
-
-    template <auto f>
-    auto get_memoized()
+    template <class ...Args, class Function>
+    auto memoize(Function&& function)
     {
-        return memo_t<f>();
+        return memoize_wrapper<Function, Args...>(std::forward<Function>(function));
     }
 }
 
+#if false
+
+#include <iostream>
+
+int main()
+{
+    std::function<int(int)> fibo_raw;
+    auto fibo = fiene::memoize<int>(fibo_raw);
+    fibo_raw = [&](int n){
+        std::cout << n << " ";
+        if(n <= 1) return n;
+        else return fibo(n-1) + fibo(n-2);
+    };
+
+    std::cout << fibo(10) << std::endl; // 10 9 8 7 6 5 4 3 2 1 0 55
+
+    class A{
+    public:
+        int data;
+        A(): data(2) {}
+        int get_data() {
+            std::cout << "Get Data! ";
+            return ++data;
+        }
+    };
+
+    A a;
+    std::function<int()> get_data_raw = std::bind(&A::get_data, &a);
+    auto get_data = fiene::memoize<>(get_data_raw);
+    std::cout << get_data() << " " << get_data(); // Get Data! 3 3
+}
+
+#endif
 #endif
